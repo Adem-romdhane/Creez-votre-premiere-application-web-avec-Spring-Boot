2670aacf41be8db2f4a834cc8e940dec322fb213
Adam Romdhane <ademromdhane@yahoo.com>
2023-05-26 17:20:45 +0200

    ajout du test getPersonsTest

--- src/test/java/oc/safetyalerts/controller/PersonControllerTest.java
+++ src/test/java/oc/safetyalerts/controller/PersonControllerTest.java
@@ -1,29 +1,23 @@
 package oc.safetyalerts.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import oc.safetyalerts.model.Person;
 import oc.safetyalerts.repository.PersonRepository;
 import oc.safetyalerts.service.PersonService;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest
 class PersonControllerTest {
@@ -33,12 +27,6 @@ class PersonControllerTest {
 
     @MockBean
     private PersonRepository personRepository;
-
-
-    @Test
-    public void testFindAllPerson() throws Exception {
-        given(personRepository.findAll());
-    }
 
     @Test
     public void getPersonsTest() {
