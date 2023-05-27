87e2aad64d4348ca3779bf995441dbd4cddc0172
Adam Romdhane <ademromdhane@yahoo.com>
2023-05-27 20:43:34 +0200

    integration test getAllMedical records

--- src/main/java/oc/safetyalerts/controller/MedicalRecordsController.java
+++ src/main/java/oc/safetyalerts/controller/MedicalRecordsController.java
@@ -1,14 +1,46 @@
 package oc.safetyalerts.controller;
 
+import oc.safetyalerts.model.FireStations;
+import oc.safetyalerts.model.MedicalRecords;
+import oc.safetyalerts.repository.MedicalRecordsRepository;
 import oc.safetyalerts.service.MedicalRecordsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class MedicalRecordsController {
 
     @Autowired
-    MedicalRecordsService medicalRecordsService;
+    MedicalRecordsRepository medicalRecordsRepository;
+
+    @GetMapping("/")
+    public List<MedicalRecords> getAllMedicalRecords() {
+        return medicalRecordsRepository.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody MedicalRecords medicalRecords) {
+        medicalRecordsRepository.save(medicalRecords);
+    }
+
+    @PutMapping(value = "/updateMedical")
+    public String updateMedicalRecords(@PathVariable Long id, @RequestBody MedicalRecords medicalRecords) {
+        MedicalRecords updateMedicalRecords = medicalRecordsRepository.findById(id).get();
+        updateMedicalRecords.setId(medicalRecords.getId());
+        updateMedicalRecords.setFirstName(medicalRecords.getFirstName());
+        updateMedicalRecords.setLastName(medicalRecords.getLastName());
+        updateMedicalRecords.setMedications(medicalRecords.getMedications());
+        updateMedicalRecords.setAllergies(medicalRecords.getAllergies());
+        updateMedicalRecords.setBirthdate(medicalRecords.getBirthdate());
+        return "Updated successfully";
+    }
+
+    @DeleteMapping(value = "/deleteMedical")
+    public void deleteMedicalRecords(@RequestBody MedicalRecords medicalRecords) {
+        medicalRecordsRepository.delete(medicalRecords);
+    }
+
 }
--- src/main/java/oc/safetyalerts/service/MedicalRecordsService.java
+++ src/main/java/oc/safetyalerts/service/MedicalRecordsService.java
@@ -1,7 +1,19 @@
 package oc.safetyalerts.service;
 
+import oc.safetyalerts.model.MedicalRecords;
+import oc.safetyalerts.repository.MedicalRecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicalRecordsService {
+
+    @Autowired
+    MedicalRecordsRepository medicalRecordsRepository;
+
+    public List<MedicalRecords> getAll(){return medicalRecordsRepository.findAll();}
+
+
 }
