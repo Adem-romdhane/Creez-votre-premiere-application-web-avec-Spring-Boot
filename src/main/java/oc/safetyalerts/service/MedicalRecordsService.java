0887bc2ff8b017e3b59424038b761fbe54ce7812
Adam Romdhane <ademromdhane@yahoo.com>
2023-06-01 16:30:58 +0200

    refactoring medicalrecord

--- src/main/java/oc/safetyalerts/service/MedicalRecordsService.java
+++ src/main/java/oc/safetyalerts/service/MedicalRecordsService.java
@@ -1,21 +1,42 @@
 package oc.safetyalerts.service;
 
+import lombok.RequiredArgsConstructor;
 import oc.safetyalerts.model.MedicalRecords;
 import oc.safetyalerts.repository.MedicalRecordsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MedicalRecordsService {
 
-    @Autowired
-    MedicalRecordsRepository medicalRecordsRepository;
+
+    private  final  MedicalRecordsRepository medicalRecordsRepository;
 
     public List<MedicalRecords> getAll(){return medicalRecordsRepository.findAll();}
 
 
+    public MedicalRecords addMedicalRecord(MedicalRecords medicalRecords) {
+        return medicalRecordsRepository.save(medicalRecords);
+    }
 
+    public MedicalRecords getById(Long id) {
+        return medicalRecordsRepository.findById(id).orElse(null);
+    }
 
+    public MedicalRecords updateMedical(MedicalRecords medicalRecords) {
+        MedicalRecords updateMedical = new MedicalRecords();
+        updateMedical.setFirstName(medicalRecords.getFirstName());
+        updateMedical.setLastName(medicalRecords.getLastName());
+        updateMedical.setBirthdate(medicalRecords.getBirthdate());
+        updateMedical.setMedications(medicalRecords.getMedications());
+        updateMedical.setAllergies(medicalRecords.getAllergies());
+
+        return medicalRecordsRepository.save(updateMedical);
+    }
+
+    public void DeleteById(MedicalRecords medicalRecords, Long id){
+        medicalRecordsRepository.deleteById(id);
+    }
 }
