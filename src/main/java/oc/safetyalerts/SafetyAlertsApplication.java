2b73bf64953faa4881a74257431139001cce6371
Adam Romdhane <ademromdhane@yahoo.com>
2023-08-01 18:12:45 +0100

    ajustement du fichier log

--- src/main/java/oc/safetyalerts/SafetyAlertsApplication.java
+++ src/main/java/oc/safetyalerts/SafetyAlertsApplication.java
@@ -1,15 +1,17 @@
 package oc.safetyalerts;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SafetyAlertsApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(SafetyAlertsApplication.class, args);
-
     }
 }
 
